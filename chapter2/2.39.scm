5bd3acc9acdc5b391c6d9fd04a76eb97def656ab
Shane Watson <shane@watson.io>
2013-02-05 16:23:10 -0800

    Complete exercise 2.39

---
+++ chapter2/2.39.scm
@@ -0,0 +1,16 @@
+;;;; Exercise 2.39
+
+;; Complete the following definitions of reverse (exercise 2.18) in
+;; terms of fold-right and fold-left from exercise 2.38:
+
+;; (define (reverse sequence)
+;;     (fold-right (lambda (x y) <??>) nil sequence))
+
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x))) '() sequence))
+
+;; (define (reverse sequence)
+;;     (fold-left (lambda (x y) <??>) nil sequence))
+
+(define (reverse sequence)
+  (fold-left (lambda (x y) (cons y x)) '() sequence))
